cfa0777e16970eb8e25de02d9fb5f6617e99d4c3
Greolis <greolis@hotmail.com>
2014-01-02 14:06:42 -0800

    Update WebRequester.cs

--- EventSource4Net/WebRequester.cs
+++ EventSource4Net/WebRequester.cs
@@ -12,6 +12,7 @@ namespace EventSource4Net
         public Task<IServerResponse> Get(Uri url)
         {
             var wreq = (HttpWebRequest)WebRequest.Create(url);
+            wreq.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Tablet PC 2.0; .NET4.0C; .NET4.0E)";
             wreq.Method = "GET";
             wreq.Proxy = null;
 
